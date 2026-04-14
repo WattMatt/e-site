0e25362a1ffbfd55bea732fe2551546112a879b5
Arno Mattheus <arno@wmeng.co.za>
2026-04-14 12:59:56 +0200

    feat(mobile): add PowerSyncProvider to root layout

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,16 +1,19 @@
 import { Slot } from 'expo-router'
 import { SupabaseProvider } from '../src/providers/SupabaseProvider'
 import { AuthProvider } from '../src/providers/AuthProvider'
+import { PowerSyncProvider } from '../src/providers/PowerSyncProvider'
 import { QueryProvider } from '../src/providers/QueryProvider'
 
 export default function RootLayout() {
   return (
     <SupabaseProvider>
-      <QueryProvider>
-        <AuthProvider>
-          <Slot />
-        </AuthProvider>
-      </QueryProvider>
+      <AuthProvider>
+        <PowerSyncProvider>
+          <QueryProvider>
+            <Slot />
+          </QueryProvider>
+        </PowerSyncProvider>
+      </AuthProvider>
     </SupabaseProvider>
   )
 }
